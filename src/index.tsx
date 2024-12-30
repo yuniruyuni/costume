6fa793beb3c7531cb7ad31463947a909e93c35e6
yuniruyuni <yuniruyuni@gmail.com>
2024-12-30 15:33:39 +0900

    bugfix: scrollbar was appeared for detail screen

--- src/index.tsx
+++ src/index.tsx
@@ -13,33 +13,37 @@ function MainScreen() {
 
   return (
     <div className="container mx-auto">
-      <header className="bg-slate-200 rounded-lg mt-2 mb-4 pl-4 pt-2 pb-2">
-        <h1 className="text-2xl text-sky-800">
-          <a href="https://yuniruyuni.net">yuniruyuni.net</a> &gt; ゆにコス:
-          ゆにるユニのコスチュームリスト
-        </h1>
-      </header>
-      <Detail
-        target={target}
-        onClose={() => setTarget(undefined)}
-        onPrevious={() => {
-          if (target) {
-            const index = costumes.indexOf(target);
-            if (index > 0) {
-              setTarget(costumes[index - 1]);
+      {target &&
+        <Detail
+          target={target}
+          onClose={() => setTarget(undefined)}
+          onPrevious={() => {
+            if (target) {
+              const index = costumes.indexOf(target);
+              if (index > 0) {
+                setTarget(costumes[index - 1]);
+              }
             }
-          }
-        }}
-        onNext={() => {
-          if (target) {
-            const index = costumes.indexOf(target);
-            if (index < costumes.length - 1) {
-              setTarget(costumes[index + 1]);
+          }}
+          onNext={() => {
+            if (target) {
+              const index = costumes.indexOf(target);
+              if (index < costumes.length - 1) {
+                setTarget(costumes[index + 1]);
+              }
             }
-          }
-        }}
-      />
-      <Gallery onSelect={setTarget} />
+          }}
+        />
+      }
+      {!target && (<>
+        <header className="bg-slate-200 rounded-lg mt-2 mb-4 pl-4 pt-2 pb-2">
+          <h1 className="text-2xl text-sky-800">
+            <a href="https://yuniruyuni.net">yuniruyuni.net</a> &gt; ゆにコス:
+            ゆにるユニのコスチュームリスト
+          </h1>
+        </header>
+        <Gallery onSelect={setTarget} />
+      </>)}
     </div>
   );
 }
